8c1adaa97542595219e88c5eef0279cf6033affe
AthenaKaycee <106272977+AthenaKaycee@users.noreply.github.com>
2022-07-10 17:06:05 -0500

    Add files via upload
    This program prompts the user to enter in a word or phrase. Once the user has entered their word or phrase they are then prompted to type in a word or character they would like to search for in the phrase that they just entered. If the phrase contains what they are searching for the code will return "True," if it does not, it will return "False."

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word or phrase");
+            string phrase = Console.ReadLine();
+            Console.WriteLine("Please enter the word or characters that you would like to search for");
+            Console.WriteLine("in the phrase that you just entered");
+            string search = Console.ReadLine();
+            Console.WriteLine( phrase.Contains(search));
+
+        }
+    }
+}
+ 
